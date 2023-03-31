e56224eaf2179f41269b047a8586ef44709cd390
Maryam <maryamquadir@gmail.com>
2023-03-31 13:04:04 -0700

    basic virtual drive registry

---
+++ virtual_mem_drive_registry.py
@@ -0,0 +1,12 @@
+registry = {}
+
+
+class VirtualMemDriveRegistry(type):
+    """ Bare-bones registry system. This enables creating multiple virtual in-mem drives. 
+        This enables multiple in-mem filesystems. 
+    """
+    def __call__(cls, *args, **kwargs):
+        obj = type.__call__(cls, *args, **kwargs)
+        registry[obj.name] = obj
+        print(f"Registered a new drive: {obj.name}")
+        return obj
