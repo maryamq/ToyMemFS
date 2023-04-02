0948a48a9768071556af3569ae97e8ffedcad41f
Maryam <maryamquadir@gmail.com>
2023-04-02 12:33:38 -0700

    add constants

---
+++ constants.py
@@ -0,0 +1,23 @@
+"""
+Contains constants and magic numbers
+"""
+
+
+class Commands:
+    """ All commands supported by the user interface.
+    """
+    LS = "ls"
+    MKDIR = "mkdir"
+    MK = "mk"
+    MVFILE = "mvfile"
+    FIND = "find"
+    WRITE = "write"
+    CAT = "cat"
+    RM = "rm"
+    PWD = "pwd"
+    CD = "cd"
+    HELP = "help"
+    SYS = "sys"
+    LOAD = "load"
+    ECHO = "echo"
+    UNKNOWN = "Unknown Command. Type help for a complete list."
